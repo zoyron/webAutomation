7beef86a270734104801c4b1624743626890f83c
zoyron <sgrarora3@gmail.com>
2020-04-21 18:04:06 +0530

    minor big fix

---
+++ asyncPromise.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+console.log("before");
+console.log("start");
+let fileWillBeReadPromise = fs.promises.readFile("file.html");
+
+fileWillBeReadPromise.then(function (content) {
+  console.log(content + " ");
+  console.log("finish");
+});
+
+fileWillBeReadPromise.catch(function (err) {
+  console.log(err);
+});
+
+console.log("after");
+console.log("I will execute");
