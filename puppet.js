3f29f3a282d0b6e03f6a7b08f4f24dfb4daa1d7b
zoyron <sgrarora3@gmail.com>
2020-05-04 20:58:09 +0530

    minor bug fix

---
+++ puppet.js
@@ -0,0 +1,7 @@
+let puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com");
+})();
